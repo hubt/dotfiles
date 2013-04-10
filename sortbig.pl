89c7c44696145676fdc4ff3997b983ccf400db00
hubt <hubert@chen.net>
2013-04-10 00:17:51 -0700

    fixed sortbig so it works

--- sortbig.pl
+++ sortbig.pl
@@ -1,37 +1,50 @@
 #!/usr/bin/perl
+
+#this is untested
+
 $infile = shift;
 $outfile = shift;
 die "Usage: bigsort <inputfile> <outputfile>" if ( !$infile || !$outfile);
 
-$splitlines = 10*1000*1000;
+$splitlines = 100000;
+$tmpdir = ".";
+$sortargs = "";
+
+
+
 $processors = `grep -c processor /proc/cpuinfo`;
 chomp($processors);
 
-$tmpdir = ".";
-$size = -s $infile;
-$free = `df $tmpdir`;
-($vol,$blocks,$used,$avail,$pct,$mount) =~ split(/\w+/,$free);
-if ( $avail < 2* $size ) {
-  warn "Warning: tempdir $tmpdir requires 2x free space of size of the input file ";
-}
-$free = `df $outfile`;
-($vol,$blocks,$used,$avail,$pct,$mount) =~ split(/\w+/,$free);
-if ( $avail < $size ) {
-  warn "Warning: output may fail due to free space $avail available, $size required";
+sub roll_file {
+  $filename = "$infile.part.$filenum";
+  push(@files,"$filename.sorted");
+  open(PART,">$infile.part.$filenum") || die;
+  $filenum++;
 }
 
+print "Splitting file $infile into directory $tmpdir with max lines $splitlines\n";
+open(SORTS,"| xargs -i -n 1 -P $processors sh -c 'sort -o {}.sorted {}; rm {}'") || die;
+open(IN,"<$infile") || die;
+$filenum = 0;
+$linecount = 0;
 
-print "Splitting file $infile into directory $tmpdir\n";
-print `split -l $splitlines $infile`;
-@files = <$infile.??>;
-print "Sorting " . scalar(@files). " individual files with $processors concurrent sorts\n";
-print `echo @files | xargs -n 1 -P $processors sort -o {}.sorted {}`;
-print "Deleting unsorted files\n";
-unlink @files;
-print "Doing final merge\n";
-for(@files) {
-  push(@sorted,"$_.sorted");
+roll_file();
+print SORTS "$filename\n";
+while(<IN>) {
+  print PART $_;
+  if ( $linecount++ > $splitlines ) {
+    roll_file();
+    print SORTS "$filename\n";
+    $linecount = 0;
+  }
 }
+close(PART);
+close(SORTS);
+    
+print "Waiting for sorts of " . scalar(@files). " individual files with $processors concurrent sorts\n";
 print "Doing final merge\n";
 print `sort -o $outfile -m @files`;
-
+print "Deleting temp files\n";
+for(@files) {
+  unlink $_;
+}
