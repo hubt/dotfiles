b7a29d2db13a6d715901cc03aee4137d077bccc3
Hubert Chen <hubert@chen.net>
2015-04-03 19:53:45 -0700

    added libedit for osx for tab completion

--- pythonrc.py
+++ pythonrc.py
@@ -22,7 +22,11 @@ class C(object):
 import readline
 import rlcompleter
 
-readline.parse_and_bind("tab: complete")
+if 'libedit' in readline.__doc__:
+  readline.parse_and_bind("bind ^I rl_complete")
+else:
+  readline.parse_and_bind("tab: complete")
+
 
 import calendar
 import datetime
